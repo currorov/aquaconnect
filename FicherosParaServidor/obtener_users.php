0a7034c4db1b7203887b8f47eaf0d080b38900e5
DESKTOP-R6TB0N5\Usuario <currorovira211103@gmail.com>
2024-05-09 14:03:28 +0200

    añado ficheros que deben ser puestos en el servidor

---
+++ FicherosParaServidor/obtener_users.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$conexion = new mysqli("localhost", "root", "", "aquaconnect");
+
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+$query = "SELECT * FROM user";
+
+$resultado = $conexion->query($query);
+
+// Verificar si la consulta fue exitosa
+if ($resultado) {
+    // Convertir los resultados en un array asociativo
+    $resultados = $resultado->fetch_all(MYSQLI_ASSOC);
+
+    // Devolver los resultados como JSON
+    echo json_encode($resultados);
+} else {
+    // Si hay un error en la consulta, devolver un mensaje de error
+    echo json_encode(array('error' => 'Error en la consulta SQL'));
+}
+
+// Cerrar la conexión a la base de datos
+$conexion->close();
+?>
