b33274fba3b0b5392f35246554ff27dd453bb381
DESKTOP-R6TB0N5\Usuario <currorovira211103@gmail.com>
2024-05-09 18:33:30 +0200

    cambio de textos de error a español

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Hemos enviado a tu correo electrónico el enlace para restablecer tu contraseña .',
+    'throttled' => 'Por favor, espere antes de intentarlo de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña no es valido.',
+    'user' => "No podemos encontrar un usuario con esa direccion de correo electónico",
+
+];
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -130,7 +130,7 @@
                             </div>
                       
                             <div>
-                                <button type="submit" class="custom-button">
+                                <button type="submit" class="custom-button ml-4">
                                     <span>Iniciar Sesión</span>
                                     <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <circle cx="37" cy="37" r="35.5" stroke="white" stroke-width="3"></circle>
