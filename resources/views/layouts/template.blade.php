c95f22125de9795cde99f07b7d2b76393ddaa3ef
DESKTOP-R6TB0N5\Usuario <currorovira211103@gmail.com>
2024-05-09 09:52:51 +0200

    cambio logo aplicacion en vista template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -14,7 +14,7 @@
     <header class="fixed-header">
         <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" href="#">
-                <img src="/img/logo.png" alt="Logo" class="img-fluid" style="max-height: 80px;">
+                <img src="/img/logo.png" alt="Logo" class="img-fluid" style="max-height: 80px; margin-left: 50px">
             </a>
             <div class="mx-auto text-center d-flex justify-content-center align-items-center" style="height: 80px;">
                 <span class="navbar-text h1 text-white">AquaConnect</span>
